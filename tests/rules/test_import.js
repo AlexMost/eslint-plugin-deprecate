3efe3ee20366b6630aeb0ceb3611fe6e4d692874
Egor Belov <georgy.belov@revolut.com>
2019-05-06 15:30:06 +0300

    fix

--- tests/rules/test_import.js
+++ tests/rules/test_import.js
@@ -10,17 +10,32 @@ ruleTester.run('import', rule, {
         {
             code: 'import a from "Module"',
             parser: 'babel-eslint',
-            options: ['Legacy'],
+            options: ['Legacy']
         },
         {
             code: 'const a = require("Module")',
             parser: 'babel-eslint',
-            options: ['Legacy'],
+            options: ['Legacy']
         },
         {
             code: 'const a = require("Module")',
             parser: 'babel-eslint',
-            options: ['path/to/Legacy'],
+            options: ['path/to/Legacy']
+        },
+        {
+            code: 'import a from "../../Legacy"',
+            parser: 'babel-eslint',
+            options: [{ name: 'path/to/Legacy', use: 'New' }]
+        },
+        {
+            code: 'import a from "Legacy"',
+            parser: 'babel-eslint',
+            options: [{ name: 'path/to/Legacy', use: 'New' }]
+        },
+        {
+            code: 'import a from "path/to"',
+            parser: 'babel-eslint',
+            options: [{ name: 'path/to/legacy', use: 'New' }]
         }
     ],
     invalid: [
@@ -28,55 +43,49 @@ ruleTester.run('import', rule, {
             code: 'import a from "Legacy"',
             parser: 'babel-eslint',
             options: ['Legacy'],
-            errors: [{
-                message: 'Module Legacy is deprecated.'
-            }]
+            errors: [
+                {
+                    message: 'Module Legacy is deprecated.'
+                }
+            ]
         },
         {
             code: 'import a from "Legacy"',
             parser: 'babel-eslint',
             options: [{ name: 'Legacy', use: 'New' }],
-            errors: [{
-                message: 'Module Legacy is deprecated. Use New instead.'
-            }]
-        },
-        {
-            code: 'import a from "Legacy"',
-            parser: 'babel-eslint',
-            options: [{ name: 'path/to/Legacy', use: 'New' }],
-            errors: [{
-                message: 'Module path/to/Legacy is deprecated. Use New instead.'
-            }]
+            errors: [
+                {
+                    message: 'Module Legacy is deprecated. Use New instead.'
+                }
+            ]
         },
         {
             code: 'import a from "path/to/Legacy"',
             parser: 'babel-eslint',
             options: [{ name: 'path/to/Legacy', use: 'New' }],
-            errors: [{
-                message: 'Module path/to/Legacy is deprecated. Use New instead.'
-            }]
-        },
-        {
-            code: 'import a from "../../Legacy"',
-            parser: 'babel-eslint',
-            options: [{ name: 'path/to/Legacy', use: 'New' }],
-            errors: [{
-                message: 'Module path/to/Legacy is deprecated. Use New instead.'
-            }]
+            errors: [
+                {
+                    message: 'Module path/to/Legacy is deprecated. Use New instead.'
+                }
+            ]
         },
         {
             code: 'var a = require("Legacy")',
             options: ['Legacy'],
-            errors: [{
-                message: 'Module Legacy is deprecated.'
-            }]
+            errors: [
+                {
+                    message: 'Module Legacy is deprecated.'
+                }
+            ]
         },
         {
             code: 'var a = require("Legacy")',
             options: [{ name: 'Legacy', use: 'New' }],
-            errors: [{
-                message: 'Module Legacy is deprecated. Use New instead.'
-            }]
-        },
+            errors: [
+                {
+                    message: 'Module Legacy is deprecated. Use New instead.'
+                }
+            ]
+        }
     ]
 });
